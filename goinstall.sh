536efedc067f017530d9e9a0350a8c802a144d65
root <root@seapeak-ThinkPad-X220.(none)>
2013-12-26 09:29:42 +0800

    add the go sh

---
+++ goinstall.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/sh
+go get github.com/bradfitz/goimports
+go get -u github.com/nsf/gocode
+go get code.google.com/p/rog-go/exp/cmd/godef
+go get github.com/jstemmer/gotags
+go get github.com/bradfitz/goimports
+go get code.google.com/p/go.tools/cmd/goimports
+go get code.google.com/p/go.tools/cmd/goimports
+go get code.google.com/p/go-tour/gotour
